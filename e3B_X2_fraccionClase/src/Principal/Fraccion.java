321c16bb62a0df0d3cf46a70485cfbca3927fcf9
asierwave <125518587+asierwave@users.noreply.github.com>
2025-10-10 19:17:47 +0200

    Fraccioens

--- e3B_X2_fraccionClase/src/Principal/Fraccion.java
+++ e3B_X2_fraccionClase/src/Principal/Fraccion.java
@@ -1,5 +1,7 @@
 package Principal;
 
+import java.util.Scanner;
+
 public class Fraccion {
 
 	
@@ -7,10 +9,108 @@ public class Fraccion {
 	private int numerador;
 	private int denominador;
 	
+	
+	//Métodos constructores
+	
 	public Fraccion (int n, int d)
 	{
 		this.numerador = n;
 		this.denominador = d;
 		
 	}
+	
+	public Fraccion (Scanner sc) {
+		System.out.println("Introduzca numerador");
+		this.numerador = sc.nextInt();
+		System.out.println("Introduzca denominador");
+		this.denominador = sc.nextInt();
+		
+	}
+	
+	
+	//Métodos para hacer operaciones aritméticas
+	
+	public Fraccion sumar (Fraccion f) {
+		Fraccion resultado; //Fracción resultado
+		int p,q; //Numeradoe y denominador del resultado
+		
+		p = this.numerador* f.denominador +f.numerador * this.denominador;
+		q = this.denominador * f.denominador;
+		
+		resultado = new Fraccion (p,q);
+		return resultado;
+			
+				}
+	
+	
+	public Fraccion restar (Fraccion f) {
+		Fraccion resultado; //Fracción resultado
+		int p,q; //Numerador y denominador del resultado
+		
+		p = this.numerador* f.denominador - f.numerador * this.denominador;
+		q = this.denominador * f.denominador;
+		
+		resultado = new Fraccion (p,q);
+		return resultado;
+				}
+	
+	
+	public Fraccion multiplicar(Fraccion f) {
+		Fraccion resultado;
+		int p,q;
+		
+		p = this.numerador * f.numerador;
+		q = this.denominador * f.denominador;
+		
+		resultado = new Fraccion (p,q);
+		return resultado;
+	}
+	
+	
+	public Fraccion cociente (Fraccion f) {
+		Fraccion resultado;
+		int p,q;
+		
+		p = this.numerador * f.denominador;
+		q = this.denominador * f.numerador;
+		
+		resultado = new Fraccion (p,q);
+		return resultado;
+		
+	}
+	
+	
+	//Métodos getters and setters
+	
+
+	public int getNumerador() {
+		return numerador;
+	}
+
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
+
+	public int getDenominador() {
+		return denominador;
+	}
+
+	public void setDenominador(int denominador) {
+		this.denominador = denominador;
+	}
+	
+	
+	
+	
+	
+	//Método string
+	
+	public String toString() {  //No recibe nada
+		
+	float valor = (float) this.numerador / this.denominador;
+	return this.numerador + "/"+this.denominador + "("+valor+")";
+
+	}
+
+	
 }
--- e3B_X2_fraccionClase/src/Principal/Main.java
+++ e3B_X2_fraccionClase/src/Principal/Main.java
@@ -1,9 +1,34 @@
 package Principal;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduce la primera fraccion:");
+		Fraccion f1 = new Fraccion(teclado);
+		System.out.println("Introduce la segunda fraccion:");
+		Fraccion f2 = new Fraccion(teclado);
+
+		// Fraccion f1 = new Fraccion(1,2);
+		// Fraccion f2 = new Fraccion(2,3);
+		Fraccion f3;
+
+		f3 = f1.sumar(f2);
+		System.out.println(f1+ " + " + f2 + " = " + f3);
+
+		f3 = f1.restar(f2);
+		System.out.println(f1+ " - " + f2 + " = " + f3);
+
+
+		f3 = f1.multiplicar(f2);
+		System.out.println(f1+ " * " + f2 + " = " + f3);
+
+		f3 = f1.cociente(f2);
+		System.out.printf(f1+ " / " + f2 + " = " + f3);
+		teclado.close();
 
 	}
 
